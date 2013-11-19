39565cab38bf6673403a297f84c45e1878b5cbad
Ben Terner <bt3ze@virginia.edu>
2013-11-18 19:29:37 -0500

    edges

--- boruvka.rs
+++ boruvka.rs
@@ -2,21 +2,30 @@ extern mod std;
 extern mod extra;
 
 struct ChildNode {
-    x: int;
-    y: int;
-    
+    pixel: Pixel;
+    parent: ParentNode;
 }
 
 impl ChildNode {
 
 }
 
+struct Edge {
+    source: &ChildNode;
+    dest: &ChildNode;
+}
+
 struct ParentNode {
-    // will need a port,chan here
+    edges: ~[Edge];
 }
 
 struct Pixel {
     r: int;
     g: int;
     b: int;
+}
+
+fn main() {
+    let pixels: ~[Pixel] = processImage();
+
 }
