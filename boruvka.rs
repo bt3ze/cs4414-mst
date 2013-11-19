5114c3f65e62e01cd55fc954bc2cd5bfe24d41ef
Ben Terner <bt3ze@virginia.edu>
2013-11-18 19:17:44 -0500

    starting to add boruvka data structures.

---
+++ boruvka.rs
@@ -0,0 +1,19 @@
+extern mod std;
+extern mod extra;
+
+struct ChildNode {
+    r: int;
+    g: int;
+    b: int;
+    x: int;
+    y: int;
+    
+}
+
+impl ChildNode {
+
+}
+
+struct ParentNode {
+    // will need a port,chan here
+}
