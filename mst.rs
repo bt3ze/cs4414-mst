c3a36f1487496a51826a8cc0fdc5b40c4fb0e3d9
Ben Terner <bt3ze@virginia.edu>
2013-11-22 12:58:51 -0500

    something that coing that compiles as a breaking point

--- mst.rs
+++ mst.rs
@@ -11,50 +11,47 @@ A parallel MST implementation on images in Rust
 
 extern mod extra;
 
-use std::{os, vec,int};
+use std::*;
 use extra::arc;
 
 struct Pixel{
     a: int
 }
 
-fn readImage( filename:  &[~str]) -> ~ [ ~[Pixel]]{
+fn readImage( filename: &~str) -> ~ [ ~[Pixel]] {
     ~[~[]]
 }
 
 fn main(){
     let argv =  os::args();
-    println(argv);
-    let length = match std::int::from_string(argv[1]){
+    println(argv.to_str());
+    let length = match from_str::<int>(argv[1]) {
         Some(x) => x,
         None() => 0
     };
-    let width =  match std::int::from_string(argv[2]){
+    let width = match from_str::<int>(argv[2]) {
         Some(x) => x,
         None() => 0
     };
-
-    let mut find_pixels: ~[ ~[Pixel] ] = readImage(argv[0]);
+    
+    let mut find_pixels: ~[ ~[Pixel] ] = readImage(&argv[0]);
     let mut find_arcs: ~[ ~[arc::RWArc<Pixel>] ] =  ~[ ~[]];
     let mut len = 0;
     let mut wid = 0;
     loop {
         loop {
             find_arcs[len][wid] = arc::RWArc::new(find_pixels[len][wid]);
-            wid ++;
-            if(wid == width)
-                break;
+                wid+=1;
+            if(wid == width) {
+                break; 
+            }
         }
-        len ++;
-        if(len == length)
-            break;
+        len+=1;
+        if(len == length) {
+            break; 
+        }
     }
-
-/*    for i = 0 to length {
-        for j = 0 to width {
-            find_arcs[i][j] = arc::RWArc::new(find_pixels[i][j];
-        }
-    } */
+    
     let arcs = find_arcs; // now we have an immutable array that can be shared
     
 }
