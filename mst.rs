3b29fbf41de62ebba2524d1b63658f18e237f05c
Ben Terner <bt3ze@virginia.edu>
2013-11-23 12:28:39 -0500

    adding beginnings of Prim's algrithm

--- mst.rs
+++ mst.rs
@@ -10,14 +10,14 @@ A parallel MST implementation on images in Rust
 
 
 extern mod extra;
-
 use std::{os, num,str} ;
-use extra::arc;
+use extra::{arc,priority_queue};
 
 struct Pixel{
     r: int,
     b: int,
-    g: int
+    g: int,
+
 }
 
 impl Pixel {
@@ -54,9 +54,7 @@ impl Ord for Edge {
 
 fn edgeCost(a: Pixel, b: Pixel) -> float {
     num::sqrt( (
-            a.r*a.r - b.r*b.r +
-            a.g*a.g - b.g*b.g +
-            a.b*a.b - b.b*b.b ) as float)
+            a.r*a.r-b.r*b.r + a.g*a.g-b.g*b.g + a.b*a.b-b.b*b.b ) as float)
 }
 
 fn readImage( filename: &~str) -> ~ [ ~[Pixel]] {
@@ -66,22 +64,22 @@ fn readImage( filename: &~str) -> ~ [ ~[Pixel]] {
 fn main(){
     let argv =  os::args();
     println(argv.to_str());
-    let length = match from_str::<int>(argv[1]) {
+    let length = match from_str::<int>(argv[2]) {
         Some(x) => x,
         None() => 0
     };
-    let width = match from_str::<int>(argv[2]) {
+    let width = match from_str::<int>(argv[3]) {
         Some(x) => x,
         None() => 0
     };
     
-    let mut find_pixels: ~[ ~[Pixel] ] = readImage(&argv[0]);
+    let pixels: ~[ ~[Pixel] ] = readImage(&argv[1]);
     let mut find_arcs: ~[ ~[arc::RWArc<Pixel>] ] =  ~[ ~[]];
     let mut len = 0;
     let mut wid = 0;
     loop {
         loop {
-            find_arcs[len][wid] = arc::RWArc::new(find_pixels[len][wid]);
+            find_arcs[len][wid] = arc::RWArc::new(pixels[len][wid]);
                 wid+=1;
             if(wid == width) {
                 break; 
@@ -97,5 +95,16 @@ fn main(){
     
     let mut x= 0;
     let mut y= 0;
-    
+    let mut queue: priority_queue::PriorityQueue<Edge> =  priority_queue::PriorityQueue::new();
+
+    loop { // iterate through items in the queue, which contains all of the edges/vertices
+        // add (x,y+1), (x,y-1), (x-1,y), (x+1,y) to queue if not already visited or colored
+        let neighbors = [ (x,y-1),(x+1,y), (x,y+1), (x,y-1)];
+        for &coord in neighbors.iter() {
+            // if neighbor at coord has not been visited or colored
+            let (w,z) = coord;
+            queue.push( Edge::new(pixels[x][y],pixels[w][z]) );
+        }
+    }
+
 }
